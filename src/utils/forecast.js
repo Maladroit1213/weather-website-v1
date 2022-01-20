cad515d7925f0f9ba19f5eb38fd37c1ba199d49a
Maladroit1213 <maladroit1213@googlemail.com>
2022-01-20 01:24:25 +0000

    Added wind data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,15 +1,15 @@
 const request = require('postman-request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=6c42e5369556147ea58fd89e64affee4&query='+latitude+','+longitude+'&units=m'
+    const url = 'http://api.weatherstack.com/current?access_key=6c42e5369556147ea58fd89e64affee4&query=' + latitude + ',' + longitude + '&units=m'
 
-    request({url, json: true},(error, {body}) => {              //object shorthand then object destructuring
-        if(error){
+    request({ url, json: true }, (error, { body }) => {              //object shorthand then object destructuring
+        if (error) {
             callback("Unable to connect to weather service.", undefined)
-        } else if (body.error){
+        } else if (body.error) {
             callback("Error: " + body.error.info, undefined)
         } else {
-            callback(undefined, "It's " + body.current.weather_descriptions[0].toLowerCase() + " with a temperature of " + body.current.temperature + " degrees outside, and it'll feel like " + body.current.feelslike + " degrees.")
+            callback(undefined, body.current.weather_descriptions[0] + " with a temperature of " + body.current.temperature + " degrees outside. It'll feel like " + body.current.feelslike + " degrees.\nWind speed is "+body.current.wind_speed+" km/h from the " +body.current.wind_dir+".")
         }         
     })
 }
