12479f17b263ecd6cb24ae51f306cc596843f095
Maladroit1213 <maladroit1213@googlemail.com>
2022-01-20 00:29:55 +0000

    Removed unneccessary comments

--- src/app.js
+++ src/app.js
@@ -4,16 +4,6 @@ const hbs = require('hbs')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-//const weatherData = require("./pathToMyFile.json");
-// app.get((req, res) => {
-//     req.send(weatherData);
-// });
-
-//const hbs = require('hbs')
-
-//console.log(__dirname)          //contains path where current script lives in
-//console.log(path.join(__dirname,'../public'))         //sets path to index.html
-
 const app = express()
 const port = process.env.PORT || 3000
 
@@ -29,19 +19,19 @@ hbs.registerPartials(partialsPath)
 
 
 // Set up static directory to serve
-app.use(express.static(publicDirectoryPath))   //way to customise server. static takes path to index.html
+app.use(express.static(publicDirectoryPath))   // way to customise server. static takes path to index.html
 
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather woowoo!',
-        name: 'Enjoyed by Mike'
+        name: 'Remember tryin hard not to piss ourselves laughin!'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About da hotties',
-        name: 'Licked by Mike'
+        name: 'Like a pig in shit'
     })
 })
 
@@ -68,12 +58,12 @@ app.get('/weather', (req, res) => {
     if (queryLocation) {
         geocode(queryLocation, (error, { latitude, longitude, location } = {}) => {
             if (error) {
-                return res.send({error})
+                return res.send({ error })
             }
 
             forecast(latitude, longitude, (error, forecastData) => {
                 if (error) {
-                    return res.send({error})
+                    return res.send({ error })
                 }
                 console.log(location)
                 console.log(forecastData)
@@ -91,22 +81,6 @@ app.get('/weather', (req, res) => {
 
 })
 
-// app.get('/products', (req, res) => {
-
-//     if (!req.query.search) {
-//         return res.send({   //use return here to ensure it doesn't run anything after it which would cause 2 sends and throw out error: Cannot set headers after they are sent to the client
-//             error: 'You must provide a search term'
-//         })
-//     }
-
-//     console.log(req.query.search)
-
-//     res.send({
-//         products: []
-//     })
-
-// })
-
 app.get('/help/*', (req, res) => {     // Used to give error on any unspecified page. here iot's for url off help.
     res.render('page404', {
         title: '404',
@@ -120,24 +94,6 @@ app.get('*', (req, res) => {     //important this is last as using * wildcard so
         errorMessage: 'Page not found'
     })
 })
-// app.get('', (req, res) => {     //This won't get sent now as it's getting served from above
-//     res.send('<h1>WeatherWow</h1>')  //if request is from browser, this is what's sent back to browser window
-//})                                  //this let's us configure what server should do when someone tries to get resource at specified url.
-// get method takes 2 args, first is route(partial url), 2nd arg is fn. Fn is what we want to do when someone takes route (1st arg)
-//fn has 2 args. first is object containing request information (req). 2nd is the response, contains methods customising what we send back to requestor (res)
-
-// app.get('/help', (req, res) => {
-//     res.send({
-// name: 'Mike',
-// age: 37
-//     })
-// })
-
-// app.get('/about', (req, res) => {
-//     res.send('<h1>All about what?</h1>')
-// })
-
-
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -3,7 +3,7 @@ const request = require('postman-request')
 const geocode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoibXJlbnNvbiIsImEiOiJja3lkZmdpMWEwMXF6MnZzOGRtd3c5aHNtIn0.kkhFq_2UTtOBM0_dg0dMEQ&limit=1'
 
-    request({ url: url, json: true }, (error, {body}) => {             //object shorthand then object destructuring
+    request({ url: url, json: true }, (error, { body }) => {             //object shorthand then object destructuring
         if (error) {
             callback('Unable to connect to location services.', undefined)
         } else if (body.features.length === 0) {
@@ -14,7 +14,7 @@ const geocode = (address, callback) => {
                 longitude: body.features[0].center[0],
                 location: body.features[0].place_name
             })
-        } 
+        }
     })
 }
 
